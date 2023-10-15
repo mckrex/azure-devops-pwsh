d09f69c665d566ba5ca780d95aa660c4db825234
mckrecker <31594382+mckrecker@users.noreply.github.com>
2023-10-14 19:53:37 -0500

    add comment based help

--- TransformAppSettings.ps1
+++ TransformAppSettings.ps1
@@ -1,3 +1,29 @@
+<#
+  .SYNOPSIS
+  Transform a settings .json file using logic similar to that used to transform .xml configuration files.
+
+  .DESCRIPTION
+  .Net Core use .json files for external app configuration (typically appsettings.json). The standard means to transform a settings file
+  deploying to various environments is to overlay some or all of a base settings file with a file matched to an environemnr. For example, when 
+  deploying to QA, at startup, the application can overlay appsettinfs.json with a file called appsettings.QA.json, then load those settings 
+  into memory.
+
+  For various reasons, an organization may need to employ logic more similar to that used for transforming XML configuration files. This script 
+  fulfills that goal producing a single file that can be copied to the server before application startup. It is specifically created for use in
+  Azure DevOps release pipelines and is constructed to be called by a PowerShell pipeline task.
+
+  The script uses a pipe character in the json property name to indicate the transformation action. The available actions are defined in the 
+  TransformType enum.
+
+  .PARAMETER FileSource
+  A directory containing a base json settings file and files to be used for it's transformation. Defaults to an environment vairable of the same 
+  name.
+
+  .PARAMETER Environment
+  For transformations when deploying to an environment, the environment name to be used to find a transform file.
+#>
+
+
 param (
     $FileSource = $env:FILESOURCE, 
     $Environment = $env:ENVIRONMENT
@@ -8,6 +34,14 @@ if ($host.Version.Major -eq 7) {
     $PSStyle.OutputRendering = [System.Management.Automation.OutputRendering]::PlainText;
 }
 
+<#
+.DESCRIPTION
+Holds values used in a tranformation file that indicate the changes to the base file. The available actions are:
+REPLACE: replace a property completely, including nested properties
+MERGE: for arrays; merges new values into an exisiting object
+ADD: adds a property not present in the base file
+REMOVE: removes a property from the base settings file
+#>
 enum TransformType {
     REPLACE
     MERGE
@@ -17,6 +51,17 @@ enum TransformType {
 
 
 function Edit-SettingsObject{
+    param(
+        [PSObject]
+        #the base settings object created by reading a settings content file
+        $baseObject, 
+        [PSObject]
+        #the settings object created holding the properties to use to transform the base object
+        $transformObject, 
+        [Int32]
+        #the depth to search for properties to tranform
+        $level
+        )
     param($baseObject, $transformObject, $level)
     $baseObject.PSObject.Properties | ForEach-Object {
         Write-Host "loop base      -> $($_.Name)"
@@ -63,10 +108,18 @@ function Edit-SettingsObject{
 }
 
 <#
-performs settings transformations
+.DESCRIPTION
+Alters a base settings file with a transform file. The content of each file is laoded into memory and mosfied by the Edit-SettingsObject function.
 #>
 function Edit-AppSettings {
-    param($transformFile, $baseFile)
+    param(
+        [string]
+        #the content file to be transformed
+        $baseFile,
+        [string]
+        #the content file used to transform the base file
+        $transformFile
+        ) 
     [string]::Format("Applying values to base settings file {0} using {1}", $baseFile.Name, $transformFile.Name) | Write-Host 
     # load the content of the base settings file and the transform settings file
     try {
